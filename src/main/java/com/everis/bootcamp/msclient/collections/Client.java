5078e044da09e9eca91dfbf34e19945aceeb115f
FredyGit <ehuachac@everis.com>
2021-08-03 19:49:34 -0500

    Create Class Cliente

---
+++ src/main/java/com/everis/bootcamp/msclient/collections/Client.java
@@ -0,0 +1,11 @@
+package com.everis.bootcamp.msclient.collections;
+
+import lombok.Data;
+
+@Data
+public class Client {
+
+    private String idClient;
+    private String name;
+    private String lastname;
+}
